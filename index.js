fee4135a84a8eb2196e65613700e3cb2e4a1586b
MD AL IMRAN <anjuarabegum1978@gmail.com>
2022-06-24 02:29:50 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,48 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import authRouter from "./routes/auth.js";
+import hotelsRouter from "./routes/hotels.js";
+import roomsRouter from "./routes/rooms.js";
+import usersRouter from "./routes/users.js";
+import cookieParser from "cookie-parser";
+const app = express();
+dotenv.config();
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    throw error;
+  }
+};
+
+mongoose.connection.on("disconnected", () => {
+  console.log("mongoDB disconnected");
+});
+
+// middleware
+app.use(cookieParser());
+app.use(express.json());
+
+app.use("/api/auth", authRouter);
+app.use("/api/hotels", hotelsRouter);
+app.use("/api/rooms", roomsRouter);
+app.use("/api/users", usersRouter);
+
+app.use((err, req, res, next) => {
+  const errorStatus = err.status || 500;
+  const errorMessage = err.message || "Something went wrong!";
+  return res.status(errorStatus).json({
+    success: false,
+    status: errorStatus,
+    message: errorMessage,
+    stack: err.stack,
+  });
+});
+
+app.listen(8080, () => {
+  connect();
+  console.log("Server is running on port 8080...");
+});
